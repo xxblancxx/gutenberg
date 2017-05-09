e39fcf6a7d6445fadc07036aad26720aa2016b88
xxblancxx <xxblancxx@hotmail.com>
2017-05-10 00:09:20 +0200

    Added UnitTests with mock for GetCitiesInTitle

--- GutenbergTests/BooksTests.cs
+++ GutenbergTests/BooksTests.cs
@@ -68,6 +68,62 @@ namespace GutenbergTests
                 Assert.AreEqual(expectedBooks[i].Title, books[i].Title);
             }
         }
+
+        /// <summary>
+        /// Test for method that gets a list of all cities mentioned within a book title - Mysql
+        /// </summary>
+        /// <param name="city"></param>
+        /// <precondition>A book title is input</precondition>
+        /// <action>Mysql database is called through facade layer</action>
+        /// <postcondition>A list of cities is given, containing geolocations</postcondition>
+        [TestMethod]
+        public void GetCitiesInTitleMysql()
+        {
+            var expectedCities = new List<City> { new City {Name="København",Latitude=1.1234,Longitude=4.1234},
+                                                  new City {Name="Odense",Latitude=2.1234,Longitude=5.1234},
+                                                  new City {Name="Roskilde",Latitude=3.1234,Longitude=6.1234}};
+
+            Mock<IDependance> mock = new Mock<IDependance>();
+            mock.Setup(o => o.Cities()).Returns(new List<City> { new City {Name="København",Latitude=1.1234,Longitude=4.1234},
+                                                                 new City {Name="Odense",Latitude=2.1234,Longitude=5.1234},
+                                                                 new City {Name="Roskilde",Latitude=3.1234,Longitude=6.1234}});
+            ConnectionFacade facade = new ConnectionFacade();
+            var cities = facade.GetCitiesInTitleMysql(mock.Object);
+            for (int i = 0; i < expectedCities.Count; i++)
+            {
+                Assert.AreEqual(expectedCities[i].Name, cities[i].Name);
+                Assert.AreEqual(expectedCities[i].Latitude, cities[i].Latitude);
+                Assert.AreEqual(expectedCities[i].Longitude, cities[i].Longitude);
+            }
+        }
+
+        /// <summary>
+        /// Test for method that gets a list of all cities mentioned within a book title - MongoDB
+        /// </summary>
+        /// <param name="city"></param>
+        /// <precondition>A book title is input</precondition>
+        /// <action>MongoDB database is called through facade layer</action>
+        /// <postcondition>A list of cities is given, containing geolocations</postcondition>
+        [TestMethod]
+        public void GetCitiesInTitleMongoDB()
+        {
+            var expectedCities = new List<City> { new City {Name="København",Latitude=1.1234,Longitude=4.1234},
+                                                  new City {Name="Odense",Latitude=2.1234,Longitude=5.1234},
+                                                  new City {Name="Roskilde",Latitude=3.1234,Longitude=6.1234}};
+
+            Mock<IDependance> mock = new Mock<IDependance>();
+            mock.Setup(o => o.Cities()).Returns(new List<City> { new City {Name="København",Latitude=1.1234,Longitude=4.1234},
+                                                                 new City {Name="Odense",Latitude=2.1234,Longitude=5.1234},
+                                                                 new City {Name="Roskilde",Latitude=3.1234,Longitude=6.1234}});
+            ConnectionFacade facade = new ConnectionFacade();
+            var cities = facade.GetCitiesInTitleMongoDB(mock.Object);
+            for (int i = 0; i < expectedCities.Count; i++)
+            {
+                Assert.AreEqual(expectedCities[i].Name, cities[i].Name);
+                Assert.AreEqual(expectedCities[i].Latitude, cities[i].Latitude);
+                Assert.AreEqual(expectedCities[i].Longitude, cities[i].Longitude);
+            }
+        }
     }
 
     public class ConnectionFacade
@@ -80,11 +136,20 @@ namespace GutenbergTests
         {
             return objectThatITalkTo.Books();
         }
+        public virtual List<City> GetCitiesInTitleMysql(IDependance objectThatITalkTo)
+        {
+            return objectThatITalkTo.Cities();
+        }
+        public virtual List<City> GetCitiesInTitleMongoDB(IDependance objectThatITalkTo)
+        {
+            return objectThatITalkTo.Cities();
+        }
     }
 
     public interface IDependance
     {
         List<Book> Books();
+        List<City> Cities();
     }
 
     public class Book
@@ -92,8 +157,9 @@ namespace GutenbergTests
         public string Title { get; set; }
     }
 
-    public class Geolocation
+    public class City
     {
+        public string Name { get; set; }
         public double Latitude { get; set; }
         public double Longitude { get; set; }
     }

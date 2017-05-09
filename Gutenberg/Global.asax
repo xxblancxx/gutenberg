0d640f0f5fbe1c8ceca89b9db08b79a8e0bf40e5
xxblancxx <xxblancxx@hotmail.com>
2017-05-09 14:25:27 +0200

    Project Initialization

---
+++ Gutenberg/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Gutenberg.Global" Language="C#" %>
---
+++ GutenbergTests/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace GutenbergTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}

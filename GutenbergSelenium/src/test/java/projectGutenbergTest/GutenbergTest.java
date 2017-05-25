82f371d043113dd9694351973f4fefc02337b822
xxblancxx <xxblancxx@hotmail.com>
2017-05-25 17:12:42 +0200

    Newest version of selenium - will use this working directory from now

--- GutenbergSelenium/src/test/java/projectGutenbergTest/GutenbergTest.java
+++ GutenbergSelenium/src/test/java/projectGutenbergTest/GutenbergTest.java
@@ -27,7 +27,7 @@ import org.openqa.selenium.Keys;
 import static projectGutenbergSelenium.Gutenberg.Compare;
 /**
  *
- * @author Casper A. Jørgensen, Martin B. Rasmussen, Søren B. Sørensen
+ * @author Manse
  */
 @FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class GutenbergTest {
